530ce3266c4ecffc9162b669296378fd56affe48
Chyjinn <chyjinn@gmail.com>
2023-09-28 22:10:08 +0200

    JS

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 require('./token.js');
+require('./seats.js');
+require('helicam');
--- token.js
+++ token.js
@@ -1,9 +1,22 @@
-mp.events.add('js:saveToken', (token) => {
-	mp.storage.data.token = token;
+mp.events.add('js:SaveToken', (accid,token,expiration) => {
+	mp.storage.data.account = String(accid);
+	mp.storage.data.token = String(token);
+	mp.storage.data.expiration = String(expiration);
+	mp.storage.flush();
 });
 
 
-mp.events.add('js:loadToken', () => {
+mp.events.add('js:LoadToken', () => {
+	var account = mp.storage.data.account;
 	var token = mp.storage.data.token;
-	mp.events.callLocal("client:LoadToken", token);
+	var expiration = mp.storage.data.expiration;
+	if( account == null || token == null || expiration == null)
+	{
+		mp.events.callLocal("client:LoadToken", "0", "tokenerror", "0");
+	}
+	else
+	{
+		mp.events.callLocal("client:LoadToken", account, token, expiration);
+	}
+	
 });

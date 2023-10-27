09b2488bd6b7c92cfa3bbf0664c7b0229cbe6b7f
morcsog <morcsog99@gmail.com>
2023-10-27 21:24:08 +0200

    AnimSelector CEF és Kliens
    Összeköttetésben a Kliens és a Cef oldal.

--- animselector/animselector.js
+++ animselector/animselector.js
@@ -6,13 +6,12 @@ let currentY;
 let osszeg = 0;    
 let nmbr = 0;
 let indexNmbr = 0;
-let indexOsszeg = 0;
+let indexOsszeg = -1;
 let animName2 = "";
 let animDictionary2 = "";
 
 const panel = document.getElementById("animSelectorPanel");
 const texts = document.getElementById("texts");
-
 texts.addEventListener("mousedown", (e) => {
   isDragging = true;
   initialX = e.clientX - panel.getBoundingClientRect().left;
@@ -33,40 +32,51 @@ document.addEventListener("mouseup", () => {
   isDragging = false;
 });
 
-function getAnimNameAndDictionary(animNames, animDictionaries){
+function getAnimNameAndDictionary(animDictionaries, animNames){
     animName2 = animNames;
     animDictionary2 = animDictionaries;
+    document.getElementById("animDictionary").innerText = animDictionary2;
+    document.getElementById("animName").innerText = animName2;
+}
+
+
+function onLoad(){
+    mp.trigger('returnAnimtoJs', 0, 0);
+    document.getElementById("animDictionary").innerText = animDictionary2;
+    document.getElementById("animName").innerText = animName2;
 }
 
 function replyClick(id){    
-    if(id == "up"){
+    if(id == "up"){        
         nmbr = parseInt(document.getElementById("flag").innerText);
         osszeg = nmbr + 1                            
-        document.getElementById("flag").innerText = osszeg;
+        document.getElementById("flag").innerText = osszeg;        
         mp.trigger('getFlagAndIdFromJs',indexOsszeg, osszeg);
                
     }else if(id == "down"){
-        if(parseInt(document.getElementById("flag").innerText) > 0){
+        if(parseInt(document.getElementById("flag").innerText) > 0){            
             nmbr = parseInt(document.getElementById("flag").innerText);
             osszeg = nmbr + -1            
-            document.getElementById("flag").innerText = osszeg;
+            document.getElementById("flag").innerText = osszeg;            
             mp.trigger('getFlagAndIdFromJs',indexOsszeg, osszeg);                        
             
         }
-    }else if(id == "left"){
-        if(parseInt(document.getElementById("index").innerText) > 0){
+    }else if(id == "left"){        
+        if(parseInt(document.getElementById("index").innerText) > 0){                        
             indexNmbr = parseInt(document.getElementById("index").innerText);
             indexOsszeg = indexNmbr - 1;            
-            document.getElementById("index").innerText = indexOsszeg;
+            document.getElementById("index").innerText = indexOsszeg;                        
             mp.trigger('getFlagAndIdFromJs',indexOsszeg, osszeg);
+            mp.trigger('returnAnimtoJs', indexOsszeg, osszeg);            
             
         }
         
-    }else if(id == "right"){
+    }else if(id == "right"){                        
         indexNmbr = parseInt(document.getElementById("index").innerText);
         indexOsszeg = indexNmbr + 1;
-        document.getElementById("index").innerText = indexOsszeg;
+        document.getElementById("index").innerText = indexOsszeg;                
         mp.trigger('getFlagAndIdFromJs', indexOsszeg, osszeg);
+        mp.trigger('returnAnimtoJs', indexOsszeg, osszeg);
     }    
 }
 function addAnimToContent(animName, animDictionary, flag){

3086d1b79ae20a3e9d7b5266246d0238f22c30ae
morcsog <morcsog99@gmail.com>
2023-10-28 00:09:37 +0200

    Anim Selector panel 90%
    Update funkció megírása hiányzik.

--- animselector/animselector.js
+++ animselector/animselector.js
@@ -10,27 +10,49 @@ let indexOsszeg = -1;
 let animName2 = "";
 let animDictionary2 = "";
 
-const panel = document.getElementById("animSelectorPanel");
-const texts = document.getElementById("texts");
-texts.addEventListener("mousedown", (e) => {
-  isDragging = true;
-  initialX = e.clientX - panel.getBoundingClientRect().left;
-  initialY = e.clientY - panel.getBoundingClientRect().top;
-});
+// Make the DIV element draggable:
+dragElement(document.getElementById("animSelectorPanel"));
 
-document.addEventListener("mousemove", (e) => {
-  if (!isDragging) return;
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id)) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
 
-  currentX = e.clientX - initialX;
-  currentY = e.clientY - initialY;
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
 
-  panel.style.left = currentX + "px";
-  panel.style.top = currentY + "px";
-});
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
 
-document.addEventListener("mouseup", () => {
-  isDragging = false;
-});
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 function getAnimNameAndDictionary(animDictionaries, animNames){
     animName2 = animNames;
@@ -45,7 +67,48 @@ function onLoad(){
     document.getElementById("animDictionary").innerText = animDictionary2;
     document.getElementById("animName").innerText = animName2;
 }
-
+//Billentyű bindek amik remélem működnek is a szerveren. Update: Működnek.
+document.addEventListener('keydown', (e) =>{
+    var keyname = e.key;
+    if(keyname == "ArrowRight"){
+        indexNmbr = parseInt(document.getElementById("index").innerText);
+        indexOsszeg = indexNmbr + 1;
+        document.getElementById("index").innerText = indexOsszeg;                
+        mp.trigger('getFlagAndIdFromJs', indexOsszeg, osszeg);
+        mp.trigger('returnAnimtoJs', indexOsszeg, osszeg);
+    }
+    if(keyname == "ArrowLeft" && parseInt(document.getElementById("index").innerText) > 0){
+        indexNmbr = parseInt(document.getElementById("index").innerText);
+        indexOsszeg = indexNmbr - 1;            
+        document.getElementById("index").innerText = indexOsszeg;                        
+        mp.trigger('getFlagAndIdFromJs',indexOsszeg, osszeg);
+        mp.trigger('returnAnimtoJs', indexOsszeg, osszeg);  
+    }
+    if(keyname == "ArrowUp"){
+        nmbr = parseInt(document.getElementById("flag").innerText);
+        osszeg = nmbr + 1                            
+        document.getElementById("flag").innerText = osszeg;        
+        mp.trigger('getFlagAndIdFromJs',indexOsszeg, osszeg);
+    }
+    if(keyname == "ArrowDown" && parseInt(document.getElementById("flag").innerText) > 0){
+        nmbr = parseInt(document.getElementById("flag").innerText);
+        osszeg = nmbr + -1            
+        document.getElementById("flag").innerText = osszeg;            
+        mp.trigger('getFlagAndIdFromJs',indexOsszeg, osszeg);
+    }
+    if(keyname == "Enter"){
+        mp.trigger('client:playAnim', animDictionary2, animName2, osszeg, true)
+    }
+    if(keyname == "Backspace"){
+        mp.trigger('client:playAnim', animDictionary2, animName2, osszeg, false)
+    }
+    if(keyname == "Shift"){
+        //ide jön az upload script.
+    }
+});
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Itt meg igazából a "gombokra" avagy képekre való kattintás van megírva. Léptetik az Index és a Flag számlálót illetve triggerelik a getFlagAndIdFromJs
+//metódust ami lényegében megkapja az Indexet és a Flag-et.
 function replyClick(id){    
     if(id == "up"){        
         nmbr = parseInt(document.getElementById("flag").innerText);
@@ -89,4 +152,27 @@ function addAnimToContent(animName, animDictionary, flag){
     anim.flag = flag;  
     document.getElementById('texts').appendChild(anim);
 
+}
+
+function playAnimation(id){
+    //Megkeresi a playBtn-t ID alapján, ha playBtn és rákattintanak akkor átvált Pause-ra és fordítva.
+    //Ezután triggereli a playAnimot a kliens oldalon, ami tovább triggereli a megadott adatokkal a szerver oldalt.
+    //A ki kommentelt rész cserélgeti a gombokat.
+    var playBtn = document.getElementById(id);
+    /*var pauseBtn = "kepek/pause.png";
+    var playBtnSrc = "kepek/play.png";*/
+    mp.trigger('client:playAnim', animDictionary2, animName2, osszeg, true)
+    /*if(playBtn.src.includes("kepek/play.png")){        
+        playBtn.src = pauseBtn;
+        mp.trigger('client:playAnim', animDictionary2, animName2, osszeg, true)
+    }else{
+        playBtn.src = playBtnSrc;
+        mp.trigger('client:playAnim', animDictionary2, animName2, osszeg, false)
+    }*/
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+}
+function stopAnimation(id){
+    mp.trigger('client:playAnim', animDictionary2, animName2, osszeg, false)
 }

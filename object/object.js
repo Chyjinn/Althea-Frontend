6bda230f51eba6b27b083a8d4cd1b042dcddfd6d
Chyjinn <77760013+Chyjinn@users.noreply.github.com>
2024-02-02 23:39:36 +0100

    object cuccok

--- object/object.js
+++ object/object.js
@@ -44,6 +44,8 @@ var moveButton = document.querySelector(".move");
 var rotateButton = document.querySelector(".rotate");
 var groundButton = document.querySelector(".ground");
 var saveButton = document.querySelector(".save");
+var closeButton = document.querySelector(".close");
+var speedSlider = document.querySelector(".speed");
 
 var px = document.querySelector("#px");
 var py = document.querySelector("#py");
@@ -52,6 +54,130 @@ var pz = document.querySelector("#pz");
 var rx = document.querySelector("#rx");
 var ry = document.querySelector("#ry");
 var rz = document.querySelector("#rz");
+
+px.addEventListener('input', RefreshValues);
+py.addEventListener('input', RefreshValues);
+pz.addEventListener('input', RefreshValues);
+rx.addEventListener('input', RefreshValues);
+ry.addEventListener('input', RefreshValues);
+rz.addEventListener('input', RefreshValues);
+
+var mode = 'move';
+closeButton.addEventListener('click', CloseEditor);
+saveButton.addEventListener('click', SaveObject);
+
+document.addEventListener('keydown', HandleKeyPress)
+
+speedSlider.addEventListener('input', changeSpeed);
+
+var speed = 20;
+
+function changeSpeed(event)
+{
+    speed = speedSlider.value;
+}
+function HandleKeyPress(event)
+{
+    var key = event.key;
+    console.log(key);
+    if(key == 'Enter')
+    {
+
+    }
+    else if(key == 'Backspace')
+    {
+
+    }
+    else if(key == 'r')
+    {
+        if(mode == 'move') 
+        {
+            mode = 'rotate';
+            PosX.classList.add('rotateicon');
+            PosX.classList.remove('moveicon');
+            PosY.classList.add('rotateicon');
+            PosY.classList.remove('moveicon');
+            PosZ.classList.add('rotateicon');
+            PosZ.classList.remove('moveicon');
+
+        }
+        else if(mode == 'rotate')
+        {
+             mode = 'move';
+             PosX.classList.remove('rotateicon');
+             PosX.classList.add('moveicon');
+             PosY.classList.remove('rotateicon');
+             PosY.classList.add('moveicon');
+             PosZ.classList.remove('rotateicon');
+             PosZ.classList.add('moveicon');
+        }
+    }
+    if(mode == 'move')
+    {
+        switch(key)
+        {
+            case 'ArrowUp':
+                mp.trigger('client:MoveObject', speed, 'Y');
+                break;
+            case 'ArrowDown':
+                mp.trigger('client:MoveObject', -speed, 'Y');
+                break;
+            case 'ArrowLeft':
+                mp.trigger('client:MoveObject', -speed, 'X');
+                break;
+            case 'ArrowRight':
+                mp.trigger('client:MoveObject', speed, 'X');
+                break;
+            case 'q':
+                mp.trigger('client:MoveObject', -speed, 'Z');
+                break;
+            case 'e':
+                mp.trigger('client:MoveObject', speed, 'Z');
+                break;
+        }
+    }
+    else if(mode = 'rotate')
+    {
+        switch(key)
+        {
+            case 'ArrowUp':
+                mp.trigger('client:RotateObject', speed, 'Y');
+                break;
+            case 'ArrowDown':
+                mp.trigger('client:RotateObject', -speed, 'Y');
+                break;
+            case 'ArrowLeft':
+                mp.trigger('client:RotateObject', -speed, 'X');
+                break;
+            case 'ArrowRight':
+                mp.trigger('client:RotateObject', speed, 'X');
+                break;
+            case 'q':
+                mp.trigger('client:RotateObject', -speed, 'Z');
+                break;
+            case 'e':
+                mp.trigger('client:RotateObject', speed, 'Z');
+                break;
+        }
+    }
+
+}
+
+function SaveObject(event)
+{
+    mp.trigger('client:SaveObject');
+}
+
+function CloseEditor(event)
+{
+    mp.trigger('client:CloseObjectEditor');
+}
+
+function RefreshValues(event)
+{
+    console.log(event.target.value);
+    mp.trigger('client:SetObject', event.target.id, event.target.value);
+}
 
 
 function ShowMover()
@@ -69,12 +195,26 @@ function ShowMover()
 function SwitchMode(event)
 {
     event.preventDefault();
-
+    console.log(event.target.id);
     switch(event.target.id)
     {
         case 'move':
+            mode = 'move';
+            PosX.classList.remove('rotateicon');
+            PosX.classList.add('moveicon');
+            PosY.classList.remove('rotateicon');
+            PosY.classList.add('moveicon');
+            PosZ.classList.remove('rotateicon');
+            PosZ.classList.add('moveicon');
             break;
         case 'rotate':
+            mode = 'rotate';
+            PosX.classList.add('rotateicon');
+            PosX.classList.remove('moveicon');
+            PosY.classList.add('rotateicon');
+            PosY.classList.remove('moveicon');
+            PosZ.classList.add('rotateicon');
+            PosZ.classList.remove('moveicon');
             break;
         case 'ground':
             mp.trigger('client:ObjectToGround');
@@ -87,12 +227,12 @@ function SwitchMode(event)
 
 function UpdateObjectPos(x,y,z,rotax,rotay,rotaz)
 {
-    px.value = x;
-    py.value = y;
-    pz.value = z;
-    rx.value = rotax;
-    ry.value = rotay;
-    rz.value = rotaz;
+    px.value = parseFloat((x).toFixed(5))
+    py.value = parseFloat((y).toFixed(5))
+    pz.value = parseFloat((z).toFixed(5))
+    rx.value = parseFloat((rotax).toFixed(5))
+    ry.value = parseFloat((rotay).toFixed(5))
+    rz.value = parseFloat((rotaz).toFixed(5))
 }
 
 
@@ -112,7 +252,6 @@ function MouseDown(event)
     {
         moveAxis = 'Z';
     }
-    console.log(moveAxis);
     clickStatus = true;
     currentX = event.pageX;
     currentY = event.pageY;
@@ -131,7 +270,6 @@ function MouseDown(event)
       }    
 }
 
-
 function MoveAt(pageX, pageY)
 {
     let relativeX = pageX - currentX;
@@ -147,10 +285,26 @@ function MoveAt(pageX, pageY)
             rel = relativeX;
             break;
         case 'Z':
-            rel = -relativeY;
+            if(mode == 'move')
+            {
+                rel = -relativeY;
+            }
+            else if(mode == 'rotate')
+            {
+                rel = relativeX;
+            }
             break;
     }
-    mp.trigger('client:MoveObject', rel, moveAxis);
+
+    if(mode == 'move')
+    {
+        mp.trigger('client:MoveObject', rel, moveAxis);
+    }
+    else if(mode == 'rotate')
+    {
+        mp.trigger('client:RotateObject', rel, moveAxis);
+    }
+    
 
     currentX = pageX;
     currentY = pageY;
